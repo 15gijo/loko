a325764d4021b62000bbd1ad592c9c649a31b69a
jailju <juji1007@hanmail.net>
2025-04-06 22:42:14 +0900

    init commit

---
+++ src/main/java/com/_gijo/loko/LokoApplication.java
@@ -0,0 +1,13 @@
+package com._gijo.loko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LokoApplication.class, args);
+	}
+
+}
