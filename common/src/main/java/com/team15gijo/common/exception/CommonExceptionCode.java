2f3011229a020db1beaa9113b48c3065db652d8d
jailju <juji1007@hanmail.net>
2025-04-08 04:04:33 +0900

    feat: CommonExceptionCode 추가

---
+++ common/src/main/java/com/team15gijo/common/exception/CommonExceptionCode.java
@@ -0,0 +1,22 @@
+package com.team15gijo.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommonExceptionCode implements ExceptionCode {
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 매개변수 입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 문제가 발생되었습니다."),
+    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "유효성 검사에 실패했습니다."),
+    PAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 페이지를 찾을 수 없습니다."),
+    INVALID_PAGE_NUMBER(HttpStatus.BAD_REQUEST, "잘못된 폐이지 번호 입니다."),
+    INVALID_PAGE_SIZE(HttpStatus.BAD_REQUEST, "잘못된 페이지 사이즈 입니다."),
+    UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
+    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "인가되지 않은 사용자입니다."),
+    REQUEST_TIMEOUT(HttpStatus.REQUEST_TIMEOUT, "요청 시간이 초과되었습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
