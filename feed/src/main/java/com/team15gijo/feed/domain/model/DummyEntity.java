0cf58ca80d9ea893b4212094d515ca49236b9456
JaeeunPark <pjcson8@naver.com>
2025-04-08 14:51:03 +0900

    feat: jpa auditing 오류 방지용 더미 entity 추가

---
+++ feed/src/main/java/com/team15gijo/feed/domain/model/DummyEntity.java
@@ -0,0 +1,10 @@
+package com.team15gijo.feed.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class DummyEntity {
+    @Id
+    private Long id;
+}
