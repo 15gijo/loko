0e6917a078b0cfeb6b0c87df57d2302b930bea1b
minji-git <kmjzz8168@gmail.com>
2025-04-08 22:05:38 +0900

    feat(v1): BaseEntity 적용

--- chat/src/main/java/com/team15gijo/chat/domain/model/ChatRoom.java
+++ chat/src/main/java/com/team15gijo/chat/domain/model/ChatRoom.java
@@ -1,6 +1,6 @@
 package com.team15gijo.chat.domain.model;
 
-import com.team15gijo.chat.application.dto.v1.ChatRoomResponseDto;
+import com.team15gijo.common.base.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -11,28 +11,39 @@ import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table(name = "p_chat_rooms")
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ChatRoom {
+@SQLRestriction("deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE p_chat_rooms SET deleted_at = now(), deleted_by = updated_by WHERE chat_room_id = ?")
+public class ChatRoom extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    private UUID chatRoomId;
 
     @Enumerated(EnumType.STRING)
     private ChatRoomType chatRoomType;
 
-    public ChatRoomResponseDto toResponse() {
-        return ChatRoomResponseDto.builder()
-            .id(id)
-            .chatRoomType(chatRoomType)
-            .build();
-    }
+//    public ChatRoomResponseDto toResponse() {
+//        return ChatRoomResponseDto.builder()
+//            .chatRoomId(chatRoomId)
+//            .chatRoomType(chatRoomType)
+//            .build();
+//    }
+//
+//    public static ChatRoom create(ChatRoomType chatRoomType) {
+//        ChatRoom chatRoom = ChatRoom.builder()
+//            .chatRoomType(chatRoomType)
+//            .build();
+//        return chatRoom;
+//    }
 }
--- chat/src/main/java/com/team15gijo/chat/domain/model/ChatRoomParticipant.java
+++ chat/src/main/java/com/team15gijo/chat/domain/model/ChatRoomParticipant.java
@@ -1,29 +1,39 @@
 package com.team15gijo.chat.domain.model;
 
+import com.team15gijo.common.base.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table(name = "p_chat_room_participants")
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ChatRoomParticipant {
+@SQLRestriction("deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE p_chat_room_participants SET deleted_at = now(), deleted_by = updated_by WHERE id = ?")
+public class ChatRoomParticipant extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    private UUID chatRoomId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
 
     private Long userId;
 
