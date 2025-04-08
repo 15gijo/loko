c23797bcc4d911ccc4f8fef7763eca5cd48e565c
jailju <juji1007@hanmail.net>
2025-04-08 16:47:49 +0900

    init: gateway 초기 설정

---
+++ gateway/src/main/java/com/team15gijo/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.team15gijo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.team15gijo.common", "com.team15gijo.gateway"})
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
