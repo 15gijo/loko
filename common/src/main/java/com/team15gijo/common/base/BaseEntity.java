b257e4ffbae9171e65a0a7f94cffeaea012ffdba
jailju <juji1007@hanmail.net>
2025-04-08 11:32:36 +0900

    refact: softDelete 조건 변경

--- common/src/main/java/com/team15gijo/common/base/BaseEntity.java
+++ common/src/main/java/com/team15gijo/common/base/BaseEntity.java
@@ -55,9 +55,9 @@ public abstract class BaseEntity {
     private void softDelete() {
         if (deletedAt == null) {
             deletedAt = LocalDateTime.now();
-        }
-        if (deletedBy == null) {
-            deletedBy = updatedBy;
+            if (deletedBy == null) {
+                deletedBy = updatedBy;
+            }
         }
     }
 
