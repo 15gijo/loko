5aa4d4ea4b85ea3b464084eaf9824d1123bed3ca
minji-git <kmjzz8168@gmail.com>
2025-04-08 11:53:01 +0900

    fix(v1): 공통 응답으로 변경

--- chat/src/main/java/com/team15gijo/chat/presentation/controller/v1/ChatController.java
+++ chat/src/main/java/com/team15gijo/chat/presentation/controller/v1/ChatController.java
@@ -2,23 +2,23 @@ package com.team15gijo.chat.presentation.controller.v1;
 
 import com.team15gijo.chat.application.dto.v1.ChatRoomResponseDto;
 import com.team15gijo.chat.application.service.impl.v1.ChatRoomService;
-import com.team15gijo.chat.presentation.dto.ApiResponse;
 import com.team15gijo.chat.presentation.dto.v1.ChatRoomRequest;
+import com.team15gijo.common.dto.ApiResponse;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/chats")
 public class ChatController {
@@ -36,13 +36,13 @@ public class ChatController {
     ) {
         ChatRoomResponseDto response = chatRoomService.createChatRoom(request);
         return ResponseEntity.status(HttpStatus.CREATED)
-            .body(ApiResponse.success(HttpStatus.OK.value(), response));
+            .body(ApiResponse.success("채팅방 생성되었습니다.", response));
     }
 
     @GetMapping("/rooms/{chatRoomId}")
     public ResponseEntity<ApiResponse<ChatRoomResponseDto>>  getChatRoom(@PathVariable("chatRoomId") UUID chatRoomId) {
         ChatRoomResponseDto response = chatRoomService.getChatRoom(chatRoomId);
         return ResponseEntity.status(HttpStatus.OK)
-            .body(ApiResponse.success(HttpStatus.OK.value(), response));
+            .body(ApiResponse.success("채팅방 조회되었습니다.", response));
     }
 }
