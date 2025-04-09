9fa425238cedec0689a165e454f4c78c6045d8d0
jailju <juji1007@hanmail.net>
2025-04-10 04:35:39 +0900

    init: 유저, 인증 서버 초기 설정

---
+++ auth/src/main/java/com/team15gijo/auth/AuthApplication.java
@@ -0,0 +1,14 @@
+package com.team15gijo.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(scanBasePackages = {"com.team15gijo.auth", "com.team15gijo.common"})
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
