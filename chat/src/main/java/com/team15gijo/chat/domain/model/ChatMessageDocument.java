e4c53bd03a0594bf241026167eae5487fb8d9607
minji-git <kmjzz8168@gmail.com>
2025-04-14 02:10:14 +0900

    feat(v1): 채팅메시지 필드 추가

--- chat/src/main/java/com/team15gijo/chat/domain/model/ChatMessageDocument.java
+++ chat/src/main/java/com/team15gijo/chat/domain/model/ChatMessageDocument.java
@@ -1,11 +1,14 @@
 package com.team15gijo.chat.domain.model;
 
 import com.team15gijo.chat.presentation.dto.v1.ChatMessageResponseDto;
+import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
@@ -15,13 +18,30 @@ public class ChatMessageDocument {
     @Id
     private String _id;
     private UUID chatRoomId;
+    @CreatedBy
+    @Column(name = "sender_by", nullable = false, updatable = false)
     private Long senderId;
     private String senderNickname;
     private ConnectionType connectionType;
     private String messageContent;
     private ChatMessageType chatMessageType;
+    @CreatedDate
+    @Column(name = "sent_at", nullable = false, updatable = false)
     private LocalDateTime sentAt;
     private Boolean readStatus;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+    @Column(name = "deleted_by")
+    private Long deletedBy;
+
+    // 삭제 처리 메서드
+    public void softDelete(Long deletedBy) {
+        if (deletedAt == null) {
+            deletedAt = LocalDateTime.now();
+            this.deletedBy = deletedBy;
+        }
+    }
 
     public ChatMessageResponseDto toResponse() {
         return ChatMessageResponseDto.builder()
@@ -31,6 +51,9 @@ public class ChatMessageDocument {
             .connectionType(connectionType)
             .messageContent(messageContent)
             .sentAt(sentAt)
-            .readStatus(readStatus).build();
+            .readStatus(readStatus)
+            .deletedAt(deletedAt)
+            .deletedBy(deletedBy)
+            .build();
     }
 }
--- chat/src/main/java/com/team15gijo/chat/domain/repository/ChatMessageRepository.java
+++ chat/src/main/java/com/team15gijo/chat/domain/repository/ChatMessageRepository.java
@@ -4,8 +4,11 @@ import com.team15gijo.chat.domain.model.ChatMessageDocument;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface ChatMessageRepository extends MongoRepository<ChatMessageDocument, String> {
 
+    // 삭제되지 않은 데이터 조회
+    @Query("{ 'deletedAt' : null }")
     List<ChatMessageDocument> findByChatRoomIdAndSenderId(UUID chatRoomId, Long senderId);
 }
