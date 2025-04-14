05575d7319c7a2abf6e43383de1121c2a21c8a84
jailju <juji1007@hanmail.net>
2025-04-14 12:03:36 +0900

    merge: user

---
+++ user/src/main/java/com/team15gijo/user/presentation/dto/v1/UserSearchResponseDto.java
@@ -0,0 +1,29 @@
+package com.team15gijo.user.presentation.dto.v1;
+
+import com.team15gijo.user.domain.model.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserSearchResponseDto {
+    private Long userId;
+    private String username;
+    private String nickname;
+    private String profile;
+    private String region;
+
+    public static UserSearchResponseDto from(UserEntity user) {
+        return UserSearchResponseDto.builder()
+                .userId(user.getId())
+                .username(user.getUserName())
+                .nickname(user.getNickName())
+                .profile(user.getProfile())
+                .region(user.getRegion())
+                .build();
+    }
+}
