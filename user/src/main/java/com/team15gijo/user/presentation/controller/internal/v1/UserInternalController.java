0b9930e8fbb838cf923a405e8ab1c6453455c2bc
jailju <juji1007@hanmail.net>
2025-04-14 11:47:14 +0900

    feat: 챗-유저 인터널 닉네임 존재 여부 로직 추가

--- user/src/main/java/com/team15gijo/user/presentation/controller/internal/v1/UserInternalController.java
+++ user/src/main/java/com/team15gijo/user/presentation/controller/internal/v1/UserInternalController.java
@@ -1,9 +1,12 @@
 package com.team15gijo.user.presentation.controller.internal.v1;
 
+import com.team15gijo.common.dto.ApiResponse;
 import com.team15gijo.user.application.service.UserApplicationService;
 import com.team15gijo.user.infrastructure.dto.UserFeignInfoResponseDto;
+import com.team15gijo.user.infrastructure.dto.v1.internal.UserSearchResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,15 +25,24 @@ public class UserInternalController {
     //유저 조회
     @GetMapping("/info")
     public UserFeignInfoResponseDto getUserInfo(@RequestParam("email") String identifier) {
-        log.info("user-service 연결={}", identifier);
+        log.info("auth - user 연결={}", identifier);
         return userApplicationService.getUserInfo(identifier);
+    }
+
+    //auth -> user
+    //유저 이메일 조회
+    @GetMapping("/{userId}/email")
+    String getEmailByUserId(@PathVariable("userId") Long userId) {
+        log.info("auth - user 연결={}", userId);
+        return userApplicationService.getEmailByUserId(userId);
     }
 
     //chat -> user
     //유저 닉네임 존재 여부
     @GetMapping("/{nickname}")
     public Long getUserIdByNickname(@PathVariable("nickname") String nickname) {
-        return null;
+        log.info("chat - user 연결={}", nickname);
+        return userApplicationService.getUserIdByNickname(nickname);
     }
 
 
