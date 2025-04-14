44a3611fda4f85493241d4119bd69057014453f8
jailju <juji1007@hanmail.net>
2025-04-14 11:00:39 +0900

    refact: application 유저, 게이트웨이 필요없는거파일 주석

---
+++ common/src/main/java/com/team15gijo/common/utils/http/RequestHeaderExtractor.java
@@ -0,0 +1,30 @@
+//package com.team15gijo.common.utils.http;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import lombok.extern.slf4j.Slf4j;
+//
+//@Slf4j
+//public class RequestHeaderExtractor {
+//
+//    public static Long extractUserId(HttpServletRequest request) {
+//        try {
+//            String userId = request.getHeader("X-User-Id");
+//            return (userId != null) ? Long.parseLong(userId) : null;
+//        } catch (NumberFormatException e) {
+//            log.warn(e.getMessage());
+//            return null;
+//        }
+//    }
+//
+//    public static String extractRole(HttpServletRequest request) {
+//        return request.getHeader("X-User-Role");
+//    }
+//
+//    public static String extractNickname(HttpServletRequest request) {
+//        return request.getHeader("X-User-Nickname");
+//    }
+//
+//    public static String extractRegion(HttpServletRequest request) {
+//        return request.getHeader("X-User-Region");
+//    }
+//}
