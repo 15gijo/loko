f8947c0a430deb69847d1de28a4badd6843248ab
minji-git <kmjzz8168@gmail.com>
2025-04-15 20:49:46 +0900

    feat(v1): 채팅방 삭제 서비스 및 레포지토리 수정

--- chat/src/main/java/com/team15gijo/chat/domain/repository/ChatRoomParticipantRepository.java
+++ chat/src/main/java/com/team15gijo/chat/domain/repository/ChatRoomParticipantRepository.java
@@ -4,4 +4,6 @@ import com.team15gijo.chat.domain.model.ChatRoomParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomParticipantRepository extends JpaRepository<ChatRoomParticipant, Long> {
+    void deleteByUserId(Long userId);
+    void deleteByUserIdAndDeletedAtIsNull(Long userId);
 }
