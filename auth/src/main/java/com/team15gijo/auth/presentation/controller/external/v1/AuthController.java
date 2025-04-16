fb3cef0a813ea41e750caebeae37da2dc5385b27
jailju <juji1007@hanmail.net>
2025-04-16 10:25:25 +0900

    feat: 어스 컨트롤러 - 매니저 롤부여 로직 추가

--- auth/src/main/java/com/team15gijo/auth/presentation/controller/external/v1/AuthController.java
+++ auth/src/main/java/com/team15gijo/auth/presentation/controller/external/v1/AuthController.java
@@ -1,11 +1,14 @@
 package com.team15gijo.auth.presentation.controller.external.v1;
 
 import com.team15gijo.auth.application.service.AuthApplicationService;
+import com.team15gijo.auth.infrastructure.dto.v1.internal.AdminAssignManagerRequestDto;
 import com.team15gijo.auth.presentation.dto.v1.AssignAdminRequestDto;
+import com.team15gijo.common.annotation.RoleGuard;
 import com.team15gijo.common.dto.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -26,5 +29,15 @@ public class AuthController {
     ) {
         authApplicationService.assignAdmin(token, assignAdminRequestDto);
         return ResponseEntity.ok(ApiResponse.success("관리자 권한 부여 성공"));
+    }
+
+    //매니저 롤 부여 - 관리자
+    @RoleGuard(value = "ADMIN")
+    @PatchMapping("/admin/manager-assign")
+    public ResponseEntity<ApiResponse<Void>> assignManager(
+            @Valid @RequestBody AdminAssignManagerRequestDto adminAssignManagerRequestDto
+    ) {
+        authApplicationService.assignManger(adminAssignManagerRequestDto);
+        return ResponseEntity.ok(ApiResponse.success("매니저 권한 부여 성공"));
     }
 }
