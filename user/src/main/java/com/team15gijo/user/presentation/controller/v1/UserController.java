a3b7ab5029c8f6f9b580cede1d82c2b6466139b3
jailju <juji1007@hanmail.net>
2025-04-16 10:20:54 +0900

    feat: 유저 컨트롤러 - 업데이트, 탈퇴 로직 추가

--- user/src/main/java/com/team15gijo/user/presentation/controller/v1/UserController.java
+++ user/src/main/java/com/team15gijo/user/presentation/controller/v1/UserController.java
@@ -1,4 +1,4 @@
-package com.team15gijo.user.presentation.controller.external.v1;
+package com.team15gijo.user.presentation.controller.v1;
 
 import com.team15gijo.common.annotation.RoleGuard;
 import com.team15gijo.common.dto.ApiResponse;
@@ -6,10 +6,15 @@ import com.team15gijo.common.utils.page.PageableUtils;
 import com.team15gijo.user.application.service.UserApplicationService;
 import com.team15gijo.user.domain.model.UserStatus;
 import com.team15gijo.user.presentation.dto.v1.AdminUserReadResponseDto;
-import com.team15gijo.user.presentation.dto.v1.UserReadResponseDto;
 import com.team15gijo.user.presentation.dto.v1.UserReadsResponseDto;
-import com.team15gijo.user.presentation.dto.v1.UserSignUpRequestDto;
-import com.team15gijo.user.presentation.dto.v1.UserSignUpResponseDto;
+import com.team15gijo.user.presentation.dto.v1.request.AdminUserStatusUpdateRequestDto;
+import com.team15gijo.user.presentation.dto.v1.request.UserEmailUpdateRequestDto;
+import com.team15gijo.user.presentation.dto.v1.request.UserPasswordUpdateRequestDto;
+import com.team15gijo.user.presentation.dto.v1.request.UserSignUpRequestDto;
+import com.team15gijo.user.presentation.dto.v1.request.UserUpdateRequestDto;
+import com.team15gijo.user.presentation.dto.v1.response.UserReadResponseDto;
+import com.team15gijo.user.presentation.dto.v1.response.UserSignUpResponseDto;
+import com.team15gijo.user.presentation.dto.v1.response.UserUpdateResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,7 +23,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,14 +40,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserApplicationService userApplicatoinService;
+    private final UserApplicationService userApplicationService;
 
     //회원가입
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse<UserSignUpResponseDto>> createUser(
             @RequestBody @Valid UserSignUpRequestDto userSignUpRequestDto
     ) {
-        UserSignUpResponseDto userSignUpResponseDto = userApplicatoinService.createUser(
+        UserSignUpResponseDto userSignUpResponseDto = userApplicationService.createUser(
                 userSignUpRequestDto);
         return ResponseEntity.ok(ApiResponse.success("회원가입 성공", userSignUpResponseDto));
     }
@@ -50,7 +57,7 @@ public class UserController {
     @GetMapping("/admin/{userId}")
     public ResponseEntity<ApiResponse<AdminUserReadResponseDto>> getUserForAdmin(
             @PathVariable("userId") Long userId) {
-        AdminUserReadResponseDto adminUserReadResponseDto = userApplicatoinService.getUserForAdmin(
+        AdminUserReadResponseDto adminUserReadResponseDto = userApplicationService.getUserForAdmin(
                 userId);
         return ResponseEntity.ok(ApiResponse.success("유저 단건 조회 성공", adminUserReadResponseDto));
     }
@@ -95,7 +102,7 @@ public class UserController {
             ) Pageable pageable
     ) {
         Pageable validatedPageable = PageableUtils.validate(pageable);
-        Page<AdminUserReadResponseDto> adminUserReadResponseDtoPage = userApplicatoinService.searchUsersForAdmin(
+        Page<AdminUserReadResponseDto> adminUserReadResponseDtoPage = userApplicationService.searchUsersForAdmin(
                 userId,
                 username,
                 nickname,
@@ -112,7 +119,7 @@ public class UserController {
     @GetMapping("/me")
     public ResponseEntity<ApiResponse<UserReadResponseDto>> getUser(
             @RequestHeader("X-User-Id") Long userId) {
-        UserReadResponseDto userReadResponseDto = userApplicatoinService.getUser(userId);
+        UserReadResponseDto userReadResponseDto = userApplicationService.getUser(userId);
         return ResponseEntity.ok(ApiResponse.success("내 정보 조회 성공", userReadResponseDto));
     }
 
@@ -143,7 +150,7 @@ public class UserController {
             ) Pageable pageable
     ) {
         Pageable validatedPageable = PageableUtils.validate(pageable);
-        Page<UserReadsResponseDto> userReadsResponseDtoPage = userApplicatoinService.searchUsers(
+        Page<UserReadsResponseDto> userReadsResponseDtoPage = userApplicationService.searchUsers(
                 nickname,
                 username,
                 region,
@@ -157,7 +164,64 @@ public class UserController {
     public ResponseEntity<ApiResponse<UserReadResponseDto>> getUserForUser(
             @PathVariable String nickname
     ) {
-        UserReadResponseDto userReadResponseDto = userApplicatoinService.getUserForUser(nickname);
+        UserReadResponseDto userReadResponseDto = userApplicationService.getUserForUser(nickname);
         return ResponseEntity.ok(ApiResponse.success("유저 상세 조회 성공", userReadResponseDto));
+    }
+
+    //내 정보 수정 - ALL
+    @RoleGuard(min = "USER")
+    @PatchMapping("/me")
+    public ResponseEntity<ApiResponse<UserUpdateResponseDto>> updateUser(
+            @RequestHeader("X-User-Id") Long userId,
+            @RequestBody UserUpdateRequestDto userUpdateRequestDto
+    ) {
+        UserUpdateResponseDto userUpdateResponseDto = userApplicationService.updateUser(
+                userId, userUpdateRequestDto);
+        return ResponseEntity.ok(ApiResponse.success("내 정보 수정 성공", userUpdateResponseDto));
+    }
+
+    //이메일 수정 - ALL
+    @RoleGuard(min = "USER")
+    @PatchMapping("/email")
+    public ResponseEntity<ApiResponse<Void>> updateEmailUser(
+            @RequestHeader("X-User-Id") Long userId,
+            @Valid @RequestBody UserEmailUpdateRequestDto userEmailUpdateRequestDto
+    ) {
+        userApplicationService.updateEmailUser(userId, userEmailUpdateRequestDto);
+        return ResponseEntity.ok(ApiResponse.success("이메일 업데이트 성공"));
+    }
+
+    //비밀번호 수정 - ALL
+    @RoleGuard(min = "USER")
+    @PatchMapping("/password")
+    public ResponseEntity<ApiResponse<Void>> updatePasswordUser(
+            @RequestHeader("X-User-Id") Long userId,
+            @Valid @RequestBody UserPasswordUpdateRequestDto userPasswordUpdateRequestDto
+    ) {
+        userApplicationService.updatePasswordUser(userId, userPasswordUpdateRequestDto);
+        return ResponseEntity.ok(ApiResponse.success("비밀번호 업데이트 성공"));
+    }
+
+    //유저 상태 수정 - 관리자
+    @RoleGuard(value = "ADMIN")
+    @PatchMapping("/admin/user-status")
+    public ResponseEntity<ApiResponse<Void>> updateUserStatus(
+            @Valid @RequestBody AdminUserStatusUpdateRequestDto adminUserStatusUpdateRequestDto
+    ) {
+        userApplicationService.updateUserStatus(adminUserStatusUpdateRequestDto);
+        return ResponseEntity.ok(
+                ApiResponse.success("유저 상태 변경 성공"));
+    }
+
+    //유저 상태 수정 - 매니저 -> v2 때 비즈니스 로직 고려
+
+    //회원탈퇴 -> 추후 postmapping 및 auth 로직 리팩토링 필요
+    @RoleGuard(min = "USER")
+    @DeleteMapping
+    public ResponseEntity<ApiResponse<Void>> deleteUser(
+            @RequestHeader("X-User-Id") Long userId
+    ) {
+        userApplicationService.deleteUser(userId);
+        return ResponseEntity.ok(ApiResponse.success("회원 탈퇴 성공"));
     }
 }
