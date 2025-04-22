23b29c381b6bc4e79d64b4849ea0f2ad51c5e7b6
dohdong <goodstar7381@gmail.com>
2025-04-22 14:04:34 +0900

    refactor: 9092 수정

--- post/src/main/java/com/team15gijo/post/infrastructure/config/ProducerApplicationKafkaConfig.java
+++ post/src/main/java/com/team15gijo/post/infrastructure/config/ProducerApplicationKafkaConfig.java
@@ -55,7 +55,7 @@ public class ProducerApplicationKafkaConfig {
     @Bean
     public ProducerFactory<String, PostElasticsearchRequestDto> searchProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
