40a18b392b0cb0a23f0855a7b8709d7acad75947
minji-git <kmjzz8168@gmail.com>
2025-04-25 15:20:26 +0900

    chore: 채팅 도메인 예외코드 오타 수정

--- chat/src/main/java/com/team15gijo/chat/domain/exception/ChatDomainExceptionCode.java
+++ chat/src/main/java/com/team15gijo/chat/domain/exception/ChatDomainExceptionCode.java
@@ -17,7 +17,7 @@ public enum ChatDomainExceptionCode implements ExceptionCode {
 
     SENT_AT_DATETIME_PARSE(HttpStatus.BAD_REQUEST, "sentAt 형식이 유효하지 않습니다. 'yyyy-MM-dd' 형식이어야 합니다."),
     KAFKA_DLT_PROCESS_ERROR(HttpStatus.BAD_REQUEST, "DLT 메시지 처리 중 오류가 발생했습니다."),
-    SEND_NOTIFICATION_TO_SLACK_FOR_JSON(HttpStatus.BAD_REQUEST, "슬랙 알림 전송 JSO 파싱 오류가 발생했습니다."),
+    SEND_NOTIFICATION_TO_SLACK_FOR_JSON(HttpStatus.BAD_REQUEST, "슬랙 알림 전송 JSON 파싱 오류가 발생했습니다."),
     SEND_NOTIFICATION_TO_SLACK_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "슬랙 알림 전송 예외가 발생했습니다.");
 
     private final HttpStatus httpStatus;
