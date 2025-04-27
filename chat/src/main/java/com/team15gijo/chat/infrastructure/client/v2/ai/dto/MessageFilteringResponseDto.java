f389c5cddb617e818611902395e2786025bf5d24
minji-git <kmjzz8168@gmail.com>
2025-04-28 02:36:05 +0900

    feat: 채팅 메시지 필터링 호출되는 ai feign client 설정

---
+++ chat/src/main/java/com/team15gijo/chat/infrastructure/client/v2/ai/dto/MessageFilteringResponseDto.java
@@ -0,0 +1,14 @@
+package com.team15gijo.chat.infrastructure.client.v2.ai.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageFilteringResponseDto {
+    private Boolean isHarmful; // 메시지 제한(삭제) 성공 여부
+    private Long deleteBy; // 삭제한 시스템(임의 관리자 id) -> 0L
+    private String messageContent;
+}
