645e5e09987f9672a618b289faa341e15f382ea4
jailju <juji1007@hanmail.net>
2025-04-28 10:03:08 +0900

    feat: recommend - follow 쪽 컨트롤러 및 dto 추가

--- follow/src/main/java/com/team15gijo/follow/infrastructure/persistence/querydsl/FollowQueryDslRepositoryImpl.java
+++ follow/src/main/java/com/team15gijo/follow/infrastructure/persistence/querydsl/FollowQueryDslRepositoryImpl.java
@@ -6,11 +6,13 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.team15gijo.follow.application.dto.v2.AdminFollowSearchCommand;
+import com.team15gijo.follow.application.dto.v2.FollowCursorCandidateResult;
 import com.team15gijo.follow.domain.model.FollowEntity;
 import com.team15gijo.follow.domain.model.QFollowEntity;
 import com.team15gijo.follow.presentation.dto.response.v2.AdminFollowSearchResponseDto;
 import com.team15gijo.follow.presentation.dto.response.v2.QAdminFollowSearchResponseDto;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -68,6 +70,50 @@ public class FollowQueryDslRepositoryImpl implements FollowQueryDslRepository {
                 .fetchOne();
 
         return new PageImpl<>(content, validatePageable, total != null ? total : 0);
+    }
+
+    @Override
+    public FollowCursorCandidateResult find2HopCandidateUserIds(Long userId, Long lastUserId,
+            Pageable validatePageable) {
+        //내가 팔로우 한 유저 리스트
+        List<Long> followingUserIds = jpaQueryFactory
+                .select(follow.followeeId)
+                .from(follow)
+                .where(follow.followerId.eq(userId))
+                .fetch();
+        if (followingUserIds.isEmpty()) {
+            return new FollowCursorCandidateResult(Collections.emptyList(), false);
+        }
+
+        //정렬 조건에 id 무조건 붙이기
+        List<OrderSpecifier<?>> orderSpecifiers = getAllOrderSpecifiers(validatePageable);
+        orderSpecifiers.add(new OrderSpecifier<>(Order.ASC, follow.followeeId));
+
+        //2hop 유저 리스트 가져오기
+        List<Long> candidateIds = jpaQueryFactory
+                .select(follow.followeeId)
+                .from(follow)
+                .where(
+                        follow.followerId.in(followingUserIds),
+                        follow.followeeId.ne(userId),
+                        follow.followeeId.notIn(followingUserIds),
+                        getlastUserId(lastUserId)
+                )
+                .orderBy(orderSpecifiers.toArray(new OrderSpecifier[0]))
+                .limit(validatePageable.getPageSize() + 1)
+                .fetch();
+
+        boolean hasNext = candidateIds.size() > validatePageable.getPageSize();
+
+        List<Long> sliceUserIds = hasNext
+                ? candidateIds.subList(0, validatePageable.getPageSize())
+                : candidateIds;
+
+        return new FollowCursorCandidateResult(sliceUserIds, hasNext);
+    }
+
+    private BooleanExpression getlastUserId(Long lastUserId) {
+        return lastUserId != null ? follow.followeeId.gt(lastUserId) : null;
     }
 
     private List<OrderSpecifier<?>> getAllOrderSpecifiers(Pageable validatePageable) {
