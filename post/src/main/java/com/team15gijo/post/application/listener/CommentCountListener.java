3571bfdecf214ee7519efa4aa26e7ce5c58796e7
dohdong <goodstar7381@gmail.com>
2025-04-28 17:52:03 +0900

    refactor: 코드래빗 수정

--- post/src/main/java/com/team15gijo/post/application/listener/CommentCountListener.java
+++ post/src/main/java/com/team15gijo/post/application/listener/CommentCountListener.java
@@ -3,11 +3,13 @@ package com.team15gijo.post.application.listener;
 import com.team15gijo.post.infrastructure.kafka.dto.v1.CommentCountEventDto;
 import com.team15gijo.post.application.service.v2.PostServiceV2;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CommentCountListener {
 
     private final PostServiceV2 postService;
@@ -18,15 +20,14 @@ public class CommentCountListener {
             groupId = "post-service-group"
     )
     public void onCommentCount(CommentCountEventDto event) {
-//        // 테스트용 강제 실패
-//        if (event.getDelta() == 9999) {
-//            throw new RuntimeException("DLQ 테스트용 예외 발생");
-//        }
+
         // Retry + DLQ 로직이 적용된 컨테이너 팩토리를 통해 호출됩니다.
         if (event.getDelta() > 0) {
             postService.addCommentCount(event.getPostId());
         } else if (event.getDelta() < 0) {
             postService.minusCommentCount(event.getPostId());
-        }
+        }else {
+            log.debug("Received comment count event with delta=0, no action needed: {}", event.getPostId());
+    }
     }
 }
