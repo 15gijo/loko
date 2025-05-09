b3cf64cbfe694528e97a3d88aa8b800387ed0289
jailju <juji1007@hanmail.net>
2025-05-09 14:46:51 +0900

    feat: auth 내부 컨트롤러 및 dto 추가

---
+++ auth/src/main/java/com/team15gijo/auth/presentation/dto/internal/request/v1/OAuthAuthSignUpRequestDto.java
@@ -0,0 +1,30 @@
+package com.team15gijo.auth.presentation.dto.internal.request.v1;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class OAuthAuthSignUpRequestDto {
+    String nickname;
+    String identifier;
+    String password;
+    String loginTypeName;
+    String oauthId;
+
+    @Builder
+    public OAuthAuthSignUpRequestDto(
+            String nickname,
+            String identifier,
+            String password,
+            String loginTypeName,
+            String oauthId) {
+        this.nickname = nickname;
+        this.identifier = identifier;
+        this.password = password;
+        this.loginTypeName = loginTypeName;
+        this.oauthId = oauthId;
+    }
+}
+
